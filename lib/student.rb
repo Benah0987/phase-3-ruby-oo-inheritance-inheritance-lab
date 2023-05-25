0297c10665dcbdd4f0c8c6722aec71394eff170e
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-25 14:43:05 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user.rb'
+class Student < User
+    super
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(english)
+        @knowledge =english
+    end 
+
+
 
 end
